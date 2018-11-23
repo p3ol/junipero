746c51f02ff6a6763348aa3f375043f56538e675
Ugo Stephant <ugo@poool.fr>
2018-11-23 11:28:02 +0100

    fix: fix textfield examples

--- examples/components/TextFieldPage.js
+++ examples/components/TextFieldPage.js
@@ -10,6 +10,7 @@ class TextFieldPage extends React.Component {
     this.state = {
       default: {},
       number: {},
+      unthemed: {},
       password: {},
       suffix: {},
       multiline: {},
@@ -48,22 +49,22 @@ class TextFieldPage extends React.Component {
           </div>
         </div>
 
-        <h2 className="mt-5">With theming</h2>
+        <h2 className="mt-5">Without theming</h2>
         <div className="row mt-5">
           <div className="col-6">
             <TextField
-              className="theme-default"
+              theme="none"
               required={true}
               boxed={this.props.boxed}
               error={this.props.error}
               placeholder="Label"
               disabled={this.props.disabled}
-              onChange={this.onChange.bind(this, 'themed')}
+              onChange={this.onChange.bind(this, 'unthemed')}
             />
           </div>
           <div className="col-6">
             <p>Current state :</p>
-            <pre>{ JSON.stringify(this.state.themed, null, 2)}</pre>
+            <pre>{ JSON.stringify(this.state.unthemed, null, 2)}</pre>
           </div>
         </div>
 
