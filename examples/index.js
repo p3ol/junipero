6b37c0c75b9b27239c336ef8c43546deea895937
Nicolas AUGER <nicolas@poool.fr>
2018-12-03 15:04:55 +0100

    styles: fix flag box-shadow

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { injectStyles } from '../src/utils';
+
 import App from './components/App';
+
+import styles from './theme/index.styl';
+injectStyles(styles);
 
 ReactDOM.render(<App />, document.getElementById('app'));
