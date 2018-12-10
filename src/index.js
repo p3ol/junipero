5ae194fe3585d13fde2851d3e884f3d4953ba4e0
Ugo Stephant <ugo@poool.fr>
2018-12-10 15:10:56 +0100

    chore: shorten exports

--- src/index.js
+++ src/index.js
@@ -1,48 +1,25 @@
 import { injectStyles } from './utils';
 
-import TextField from './components/TextField';
-import CodeField from './components/CodeField';
-import CheckBox from './components/CheckBox';
-import SelectField from './components/SelectField';
-import Slider from './components/Slider';
-import Toggle from './components/Toggle';
-import DateField from './components/DateField';
-import Button from './components/Button';
-import TagsField from './components/TagsField';
-import BreadCrumb from './components/BreadCrumb';
-import Tooltip from './components/Tooltip';
-import Tabs from './components/Tabs';
-import Tab from './components/Tab';
-import Modal from './components/Modal';
-import ColorPicker from './components/ColorPicker';
-import Switch from './components/Switch';
-import Dropdown from './components/Dropdown';
-import DropdownToggle from './components/DropdownToggle';
-import DropdownMenu from './components/DropdownMenu';
-import DropdownItem from './components/DropdownItem';
+export TextField from './components/TextField';
+export CodeField from './components/CodeField';
+export CheckBox from './components/CheckBox';
+export SelectField from './components/SelectField';
+export Slider from './components/Slider';
+export Toggle from './components/Toggle';
+export DateField from './components/DateField';
+export Button from './components/Button';
+export TagsField from './components/TagsField';
+export BreadCrumb from './components/BreadCrumb';
+export Tooltip from './components/Tooltip';
+export Tabs from './components/Tabs';
+export Tab from './components/Tab';
+export Modal from './components/Modal';
+export ColorPicker from './components/ColorPicker';
+export Switch from './components/Switch';
+export Dropdown from './components/Dropdown';
+export DropdownToggle from './components/DropdownToggle';
+export DropdownMenu from './components/DropdownMenu';
+export DropdownItem from './components/DropdownItem';
 
 import styles from './theme/index.styl';
 injectStyles(styles, { id: 'junipero-main-styles' });
-
-export {
-  TextField,
-  CodeField,
-  CheckBox,
-  SelectField,
-  Slider,
-  Toggle,
-  DateField,
-  Button,
-  TagsField,
-  BreadCrumb,
-  Tooltip,
-  Tabs,
-  Tab,
-  Modal,
-  ColorPicker,
-  Switch,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-};
