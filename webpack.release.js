d92f5ffe9afda6b15e5b5785e5bd72b0d9bb6c47
Ugo Stephant <ugo@poool.fr>
2019-06-03 07:44:07 +0200

    chore: update webpack config after dependency upgrade

--- webpack.release.js
+++ webpack.release.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 //   .BundleAnalyzerPlugin;
 
