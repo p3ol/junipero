82732f72355aada24fede416a0c4bc8da3403a5e
Ugo Stephant <ugo@poool.fr>
2020-05-26 19:22:08 +0200

    chore: upgrade to rollup monorepo plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,95 +1,52 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import autoprefixer from 'autoprefixer';
-import { eslint } from 'rollup-plugin-eslint';
 import { terser } from 'rollup-plugin-terser';
 
-const defaultConfig = () => ({
-  input: 'src/index.js',
+const isForIE = process.env.BABEL_ENV === 'ie';
+const input = './src/index.js';
+const output = `./dist${isForIE ? '/ie' : ''}/junipero`;
+const formats = ['umd', 'cjs', 'esm'];
+
+const defaultExternals = ['react', 'react-dom', 'prop-types', 'react-popper'];
+const defaultGlobals = {
+  react: 'React',
+  'prop-types': 'PropTypes',
+  'react-dom': 'ReactDOM',
+  'react-popper': 'ReactPopper',
+};
+
+const defaultPlugins = [
+  babel({
+    exclude: 'node_modules/**',
+    babelHelpers: 'runtime',
+  }),
+  postcss({
+    extensions: ['.styl'],
+    minimize: true,
+    inject: false,
+    plugins: [
+      autoprefixer,
+    ],
+  }),
+  resolve(),
+  commonjs(),
+  terser(),
+];
+
+export default formats.map(f => ({
+  input,
+  plugins: [
+    ...defaultPlugins,
+  ],
+  external: defaultExternals,
   output: {
+    file: `${output}.${f}.js`,
+    format: f,
     name: 'junipero',
     sourcemap: true,
+    globals: defaultGlobals,
   },
-  plugins: [
-    eslint({
-      exclude: [
-        'src/theme/**',
-      ],
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      externalHelpers: true,
-    }),
-    postcss({
-      extensions: ['.styl'],
-      minimize: true,
-      inject: false,
-      plugins: [
-        autoprefixer,
-      ],
-    }),
-  ],
-  external: [
-    'react',
-    'react-dom',
-    'prop-types',
-    'react-popper',
-  ],
-});
-
-const defaultUMDConfig = (minified = false, config = defaultConfig()) => ({
-  ...config,
-  output: {
-    ...config.output,
-    format: 'umd',
-    globals: {
-      react: 'React',
-      'prop-types': 'PropTypes',
-      'react-dom': 'ReactDOM',
-      'react-popper': 'ReactPopper',
-    },
-  },
-  plugins: [
-    ...config.plugins,
-    resolve(),
-    commonjs(),
-    minified ? terser() : null,
-  ],
-});
-
-/*
-  COMMONJS / MODULE CONFIG
-*/
-const libConfig = (config = defaultConfig()) => ({
-  ...config,
-  output: [
-    { file: 'dist/junipero.cjs.js', format: 'cjs' },
-  ],
-});
-
-/*
-  UMD CONFIG
-*/
-const umdConfig = (config = defaultUMDConfig()) => ({
-  ...config,
-  output: {
-    ...config.output,
-    file: 'dist/junipero.js',
-  },
-});
-
-const umdMinConfig = (config = defaultUMDConfig(true)) => ({
-  ...config,
-  output: {
-    ...config.output,
-    file: 'dist/junipero.min.js',
-  },
-});
-
-export default [
-  libConfig(),
-  umdConfig(),
-  umdMinConfig(),
-];
+}));
