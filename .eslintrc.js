c9fc59c048c07a6553d28061bdfce4db975aa55c
Ugo Stephant <ugo@poool.fr>
2020-05-26 19:21:05 +0200

    chore: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ['@poool/eslint-config-react'],
+  overrides: [{
+    files: ['tests/**/*.js'],
+    parser: 'babel-eslint',
+    env: {
+      jest: true,
+    },
+  }],
 };
