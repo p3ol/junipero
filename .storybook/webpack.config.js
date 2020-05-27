4c45febe28c7b509738c08c4046fc49e29aa1843
Ugo Stephant <ugo@poool.fr>
2020-05-27 14:17:17 +0200

    chore: fix styles not being added to static stories

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,8 +3,10 @@ const autoprefixer = require('autoprefixer');
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.styl$/,
+    sideEffects: true,
     use: [
       'style-loader',
+      'css-loader',
       {
         loader: 'postcss-loader',
         options: {
