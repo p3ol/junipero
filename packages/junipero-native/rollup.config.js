9fd7d18c40063d89c4f7721f6de802d5c3fd32c9
Ugo Stephant <ugo@poool.fr>
2020-07-01 12:41:06 +0200

    chore(native): fix missing build config

---
+++ packages/junipero-native/rollup.config.js
@@ -0,0 +1,53 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import path from 'path';
+
+const input = './lib/index.js';
+const output = './dist';
+const name = 'junipero-native';
+const formats = ['cjs', 'esm'];
+
+const defaultExternals = ['react', 'react-dom', 'prop-types', 'react-native'];
+const defaultGlobals = {
+  react: 'React',
+  'prop-types': 'PropTypes',
+  'react-dom': 'ReactDOM',
+  'react-popper': 'ReactPopper',
+  'react-native': 'ReactNative',
+};
+
+const defaultPlugins = [
+  babel({
+    exclude: /node_modules/,
+    babelHelpers: 'bundled',
+  }),
+  resolve(),
+  commonjs(),
+  terser(),
+];
+
+export default formats.map(f => ({
+  input,
+  plugins: [
+    ...defaultPlugins,
+  ],
+  external: defaultExternals,
+  output: {
+    ...(f === 'esm' ? {
+      dir: `${output}/esm`,
+      chunkFileNames: '[name].js',
+    } : {
+      file: `${output}/${name}.${f}.js`,
+    }),
+    format: f,
+    name,
+    sourcemap: true,
+    globals: defaultGlobals,
+  },
+  ...(f === 'esm' ? {
+    manualChunks: id =>
+      id.includes('node_modules') ? 'vendor' : path.parse(id).name,
+  } : {}),
+}));
