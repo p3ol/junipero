dac202fa2565c20b3b87ccb088775486d9fe1337
Ugo Stephant <ugo@poool.fr>
2020-07-08 10:06:16 +0200

    chore: fix babel config for tests

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   env: {
     tests: {
-      presets: ['@babel/env', '@babel/react'],
+      presets: [
+        '@babel/env',
+        '@babel/react',
+        'module:metro-react-native-babel-preset',
+      ],
       plugins: [
         ['@babel/transform-runtime', {
           regenerator: true,
