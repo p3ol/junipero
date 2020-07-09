c82b5aa94a0e01a84c3201251a0a129ba0a5f025
Ugo Stephant <ugo@poool.fr>
2020-07-09 09:23:41 +0200

    fix(native): fix missing additional exports

--- packages/junipero-native/lib/index.js
+++ packages/junipero-native/lib/index.js
@@ -1,2 +1,15 @@
 export { default as CheckboxField } from './CheckboxField';
 export { default as TextField } from './TextField';
+
+export { applyStyles } from './theme';
+
+export {
+  exists,
+  isNull,
+  isUndefined,
+} from '@poool/junipero-utils';
+
+export {
+  useEventListener,
+  useTimeout,
+} from '@poool/junipero-hooks';
