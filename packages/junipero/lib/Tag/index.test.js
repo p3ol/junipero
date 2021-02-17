b0bda273b7b44b8ee5524ab1b7e5e97f69d609b8
defless <simon.deflesschouwer@mmibordeaux.com>
2021-02-17 15:18:20 +0100

    test: add test

---
+++ packages/junipero/lib/Tag/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Tag from './';
+
+describe('<Tag />', () => {
+
+  it('should render tag component', () => {
+    const component = mount(<Tag />);
+    expect(component.find('.junipero.tag').length).toBe(1);
+  });
+});
