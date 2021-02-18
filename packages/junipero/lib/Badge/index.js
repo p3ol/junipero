0b424333f1490d49242d7e0c650d5a33d5ffd15b
defless <simon.deflesschouwer@mmibordeaux.com>
2021-02-18 15:09:02 +0100

    fix: clear code

--- packages/junipero/lib/Badge/index.js
+++ packages/junipero/lib/Badge/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { classNames } from '@poool/junipero-utils';
 
-const Badge = ({ className, ...rest }) => (
-  <span {...rest} className={classNames('junipero', 'badge', className)}/>
+export default ({ className, ...rest }) => (
+  <span {...rest} className={classNames('junipero', 'badge', className)} />
 );
-
-export default Badge;
--- packages/junipero/lib/Badge/index.test.js
+++ packages/junipero/lib/Badge/index.test.js
@@ -5,7 +5,7 @@ import Badge from './';
 
 describe('<Badge />', () => {
   it('should render a basic badge', () => {
-    const component = mount(<Badge value="1"/>);
+    const component = mount(<Badge>1</Badge>);
     expect(component.find('.junipero.badge').length).toBe(1);
   });
 });
