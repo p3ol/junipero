c7b50e645999379855ecea7e62010909eb323201
defless <simon.deflesschouwer@mmibordeaux.com>
2021-02-18 14:35:42 +0100

    test: add test

---
+++ packages/junipero/lib/Card/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Card from './';
+
+describe('<Card />', () => {
+  it('should render Card component and it child', () => {
+    const component = mount(<Card><div className="test"></div></Card>);
+    expect(component.find('.junipero.card').length).toBe(1);
+    expect(component.find('.test').length).toBe(1);
+  });
+});
