2a5b33f172a94f9227704141c0a5c8d959b518ca
Ugo Stephant <ugo@poool.fr>
2021-04-19 22:20:24 +0200

    feat(lib): better touchable zone component

---
+++ packages/junipero/lib/TouchableZone/index.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import TouchableZone from './index';
+
+export default { title: 'junipero/TouchableZone' };
+
+export const basic = () => (
+  <TouchableZone>
+    <i className="material-icons" style={{ marginRight: 10 }}>add</i>
+    <span>Add image</span>
+  </TouchableZone>
+);
