f1a4e7fc046a07d312757c483571de251d9f0f98
dackmin <ugo@poool.fr>
2021-11-08 13:20:46 +0100

    tests: fix jest configs

--- packages/junipero/jest.config.js
+++ packages/junipero/jest.config.js
@@ -4,16 +4,16 @@ module.exports = {
   displayName: 'junipero',
   clearMocks: true,
   rootDir: path.resolve(),
-  timers: 'fake',
+  timers: 'real',
   moduleNameMapper: {
     '^@poool/junipero-(.+)$': '<rootDir>/packages/junipero-$1/lib/index.js',
   },
-  testEnvironment: 'jest-environment-jsdom-fourteen',
   testMatch: ['<rootDir>/packages/junipero/lib/**/*.test.js'],
   testPathIgnorePatterns: [
     '/node_modules/',
     '/old/',
   ],
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: [
     '<rootDir>/.ci/config/enzyme.js',
     '<rootDir>/.ci/config/popper.js',
