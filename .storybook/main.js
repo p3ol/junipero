f8bd956dcef17c9383c49c3f3469abc982180ec2
dackmin <ugo@poool.fr>
2022-02-23 13:47:06 +0100

    chore: update storybook config for react 17 & webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   stories: ['../packages/**/*.stories.js'],
   addons: [
@@ -6,5 +9,38 @@ module.exports = {
   ],
   core: {
     builder: 'webpack5',
-  }
+  },
+  webpackFinal: config => {
+    config.resolve.modules = ['node_modules', path.resolve('./node_modules')];
+    config.resolve.alias = {
+      'react-native': 'react-native-web',
+    };
+
+    config.module.rules.push({
+      test: /react-native-web\/(.+).js$/,
+      use: [
+        'babel-loader',
+      ],
+    });
+  
+    config.module.rules.push({
+      test: /\.styl$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              sourceMap: true,
+              plugins: [autoprefixer],
+            },
+          },
+        },
+        'stylus-loader',
+      ],
+    });
+
+    return config;
+  },
 };
