26c774362807f5a8692c2bdd50c20917932f6345
Ugo Stephant <ugo@poool.fr>
2022-09-28 18:37:44 +0200

    refactor: clean linter issues

--- packages/react/lib/SelectField/index.test.js
+++ packages/react/lib/SelectField/index.test.js
@@ -1,5 +1,5 @@
 import { createRef } from 'react';
-import { render, act, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { blur, reset } from '~test-utils';
