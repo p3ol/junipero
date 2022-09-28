2fdd85ec13596d5930095e0d13b6f2410484b8fc
Ugo Stephant <ugo@poool.fr>
2022-09-28 18:37:27 +0200

    tests(react): add more selectfield tests

--- packages/react/lib/SelectField/index.test.js
+++ packages/react/lib/SelectField/index.test.js
@@ -1,6 +1,8 @@
 import { createRef } from 'react';
 import { render, act, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
+import { blur, reset } from '~test-utils';
 import SelectField from './index';
 
 describe('<SelectField />', () => {
@@ -43,8 +45,36 @@ describe('<SelectField />', () => {
     );
 
     fireEvent.click(getByText('Item 1'));
-    await act(async () => { ref.current.blur(); });
+    await blur(ref.current);
     expect(container).toMatchSnapshot();
+    unmount();
+  });
+
+  it('should allow to reset the field', async () => {
+    const user = userEvent.setup();
+    const ref = createRef();
+    const onChange = jest.fn();
+    const { unmount, container, getByText } = render(
+      <SelectField
+        ref={ref}
+        value="John"
+        options={['John', 'David']}
+        onChange={onChange}
+      />
+    );
+
+    const input = container.querySelector('input');
+    await user.click(input);
+    await user.click(getByText('David'));
+    await blur(ref.current);
+
+    expect(container).toMatchSnapshot();
+    expect(onChange)
+      .toHaveBeenLastCalledWith(expect.objectContaining({ value: 'David' }));
+
+    await reset(ref.current);
+    expect(container).toMatchSnapshot();
+
     unmount();
   });
 });
--- packages/react/tests/utils.js
+++ packages/react/tests/utils.js
@@ -1,3 +1,5 @@
 import { act } from '@testing-library/react';
 
 export const blur = async elmt => await act(async () => elmt.blur());
+
+export const reset = async elmt => await act(async () => elmt.reset());
