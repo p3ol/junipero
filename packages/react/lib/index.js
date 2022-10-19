05777995afce195e27a8bfd5dd97547b87049d3e
Ugo Stephant <ugo@poool.fr>
2022-10-19 12:10:37 +0200

    fix(react): missing exports

--- packages/react/lib/index.js
+++ packages/react/lib/index.js
@@ -3,17 +3,31 @@ export { default as Abstract } from './lib/Abstract';
 export { default as Alert } from './lib/Alert';
 export { default as Alerts } from './lib/Alerts';
 export { default as AlertsControl } from './lib/AlertsControl';
+export { default as Badge } from './lib/Badge';
+export { default as BreadCrumb } from './lib/BreadCrumb';
+export { default as BreadCrumbItem } from './lib/BreadCrumbItem';
+export { default as Button } from './lib/Button';
+export { default as Card } from './lib/Card';
+export { default as Draggable } from './lib/Draggable';
 export { default as Dropdown } from './lib/Dropdown';
 export { default as DropdownGroup } from './lib/DropdownGroup';
 export { default as DropdownItem } from './lib/DropdownItem';
 export { default as DropdownMenu } from './lib/DropdownMenu';
 export { default as DropdownToggle } from './lib/DropdownToggle';
+export { default as Droppable } from './lib/Droppable';
+export { default as Label } from './lib/Label';
 export { default as Spinner } from './lib/Spinner';
+export { default as Tab } from './lib/Tab';
+export { default as Tabs } from './lib/Tabs';
 export { default as Tag } from './lib/Tag';
+export { default as Tooltip } from './lib/Tooltip';
 
 // Forms
+export { default as CheckboxField } from './lib/CheckboxField';
+export { default as CodeField } from './lib/CodeField';
+export { default as ColorField } from './lib/ColorField';
+export { default as DateField } from './lib/DateField';
 export { default as FieldControl } from './lib/FieldControl';
-export { default as Label } from './lib/Label';
 export { default as SelectField } from './lib/SelectField';
 export { default as TextField } from './lib/TextField';
 
