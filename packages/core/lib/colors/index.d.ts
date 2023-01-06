a202d338ad28c7ab9a25527fc6a214ca03df03dd
Ugo Stephant <ugo@poool.fr>
2023-01-06 07:23:03 +0100

    feat(core): add core types

---
+++ packages/core/lib/colors/index.d.ts
@@ -0,0 +1,97 @@
+export function hsva2hsla({ h, s, v, a }: {
+  h: number;
+  s: number;
+  v: number;
+  a: number;
+}): {
+  h: number;
+  s: number;
+  l: number;
+  a: number;
+};
+
+export function hsla2hsva({ h, s, l, a }: {
+  h: number;
+  s: number;
+  l: number;
+  a: number;
+}): {
+  h: number;
+  s: number;
+  v: number;
+  a: number;
+};
+
+export function hsva2rgba({ h, s, v, a }?: {
+  h: number;
+  s: number;
+  v: number;
+  a?: number;
+}): {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+};
+
+export function rgba2hsva({ r, g, b, a }?: {
+  r: number;
+  g: number;
+  b: number;
+  a?: number;
+}): {
+  h: number;
+  s: number;
+  v: number;
+  a: number;
+};
+export function rgba2hex({ r, g, b }: {
+  r: number;
+  g: number;
+  b: number;
+}): string;
+
+export function denormalizeHSLA({ h, s, l, a }: {
+  h: number;
+  s: number;
+  l: number;
+  a: number;
+}): {
+  h: number;
+  s: number;
+  l: number;
+  a: number;
+};
+
+export function denormalizeHSVA({ h, s, v, a }: {
+  h: number;
+  s: number;
+  v: number;
+  a: number;
+}): {
+  h: number;
+  s: number;
+  v: number;
+  a: number;
+};
+
+export function denormalizeRGBA({ r, g, b, a }: {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}): {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+};
+
+export function parseColor(color?: string): {
+  h: number;
+  s: number;
+  v: number;
+  a: number;
+};
+
+export function stringifyColor(color: any, format?: string): string;
---
+++ packages/core/lib/numbers/index.d.ts
@@ -0,0 +1,12 @@
+export function getFloatPrecision(value: number): number;
+
+export function ensureMinMax(
+  value: number,
+  min?: number,
+  max?: number
+): number;
+
+export function formatThousand(
+  count: number,
+  separator?: string
+): string;
