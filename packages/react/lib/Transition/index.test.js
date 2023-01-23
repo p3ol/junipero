fdb21722ef0151f8c1cca73fa9d8cac33b4194c2
Ugo Stephant <ugo@poool.fr>
2023-01-23 16:58:02 +0100

    tests(react): fix transition act warnings

--- packages/react/lib/Transition/index.test.js
+++ packages/react/lib/Transition/index.test.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, act } from '@testing-library/react';
 
 import Transition from '.';
 
@@ -21,10 +21,12 @@ describe('<Transition />', () => {
     };
 
     const { container, unmount } = render(<Comp />);
-    expect(container).toMatchSnapshot();
+    expect(container).toMatchSnapshot('Idle');
     fireEvent.click(container.querySelector('button'));
-    jest.advanceTimersByTime(100);
-    expect(container).toMatchSnapshot();
+    act(() => { jest.advanceTimersByTime(50); });
+    expect(container).toMatchSnapshot('Active');
+    act(() => { jest.advanceTimersByTime(100); });
+    expect(container).toMatchSnapshot('Done');
     unmount();
   });
 });
