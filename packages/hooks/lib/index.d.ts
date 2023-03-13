9181449db48ffc033f99e4ca983a9e805946af22
Ugo Stephant <ugo@poool.fr>
2023-03-13 08:19:31 +0100

    fix(hooks): better types

--- packages/hooks/lib/index.d.ts
+++ packages/hooks/lib/index.d.ts
@@ -1,18 +1,18 @@
 export declare function useEventListener(
   name: string,
   handler: Function,
-  options?: { target?: any, enabled?: Boolean }): void;
+  options?: { target?: any, enabled?: boolean }): void;
 
 export declare function useInterval(
   cb: Function,
-  time: Number,
+  time: number,
   changes?: Array<any>,
   options?: { enabled?: boolean, layoutEffect?: boolean },
   ): void;
 
 export declare function useTimeout(
   cb: Function,
-  time: Number,
+  time: number,
   changes?: Array<any>,
   options?: { enabled?: boolean, layoutEffect?: boolean },
 ): void;
