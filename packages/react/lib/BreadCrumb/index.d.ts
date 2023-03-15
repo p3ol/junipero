ec301be3f166592ecfc6745e899817a09281cca2
Ugo Stephant <ugo@poool.fr>
2023-03-15 09:41:02 +0100

    fix(react): allow to render breadcrumb items from prop

--- packages/react/lib/BreadCrumb/index.d.ts
+++ packages/react/lib/BreadCrumb/index.d.ts
@@ -1,6 +1,7 @@
 import { ReactNode, MutableRefObject, ComponentPropsWithRef } from 'react';
 
 export declare type BreadCrumbRef = {
+  items: Array<ReactNode | JSX.Element>;
   isJunipero: boolean;
   innerRef: MutableRefObject<any>;
 };
@@ -8,6 +9,7 @@ export declare type BreadCrumbRef = {
 declare interface BreadCrumbProps extends ComponentPropsWithRef<any> {
   children?: ReactNode | JSX.Element;
   className?: string;
+  items?: Array<ReactNode | JSX.Element>;
   maxItems?: number;
   filterItem?(children: ReactNode | JSX.Element): boolean;
   ref?: MutableRefObject<BreadCrumbRef | undefined>;
