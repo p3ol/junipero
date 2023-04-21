61ecbf5aa6f3255aba85adb39ac369c7eeddf0c8
Ugo Stephant <ugo@poool.fr>
2023-04-21 15:52:00 +0200

    chore: upgrade storybook to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,20 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
-
 module.exports = {
   stories: [
     '../packages/*/lib/**/*.stories.js',
   ],
   addons: [
     '@storybook/addon-storysource',
-    '@storybook/addon-actions/register',
+    '@storybook/addon-actions',
     'storybook-dark-mode',
   ],
-  core: {
-    builder: 'webpack5',
-  },
   webpackFinal: config => {
-    config.resolve.modules = ['node_modules', path.resolve('./node_modules')];
+    config.resolve.modules = [
+      'node_modules',
+      path.resolve('./node_modules'),
+    ];
+
     config.resolve.alias = {
       'react-native': 'react-native-web',
       '@junipero/core': path.resolve('./packages/core/lib'),
@@ -25,56 +25,48 @@ module.exports = {
 
     config.module.rules.push({
       test: /react-native-web\/(.+).js$/,
-      use: [
-        'babel-loader',
-      ],
+      use: ['babel-loader'],
     });
-  
+
     config.module.rules.push({
       test: /\.styl$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              sourceMap: true,
-              plugins: [autoprefixer],
-            },
+      use: ['style-loader', 'css-loader', {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            sourceMap: true,
+            plugins: [autoprefixer],
           },
         },
-        'stylus-loader',
-      ],
+      }, 'stylus-loader'],
     });
 
     config.module.rules.push({
       test: /\.sass$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              sourceMap: true,
-              plugins: [autoprefixer],
-            },
+      use: ['style-loader', 'css-loader', {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            sourceMap: true,
+            plugins: [autoprefixer],
           },
         },
-        {
-          loader: 'sass-loader',
-          options: {
-            sassOptions: {
-              includePaths: [
-                path.resolve('./packages/theme/lib/utils'),
-              ],
-            },
+      }, {
+        loader: 'sass-loader',
+        options: {
+          sassOptions: {
+            includePaths: [
+              path.resolve('./packages/theme/lib/utils'),
+            ],
           },
         },
-      ],
+      }],
     });
 
     return config;
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
 };
