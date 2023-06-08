8778adb861ea3d9bd655b51c2981bbf95e92b30d
Simon Deflesschouwer <43884154+defless@users.noreply.github.com>
2023-06-08 17:08:32 +0200

    test: update snapshots for stepper component

--- packages/react/lib/Stepper/index.test.js
+++ packages/react/lib/Stepper/index.test.js
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 
 import Stepper from './';
+import Step from '../Step';
 
 describe('<Stepper />', () => {
 
@@ -10,19 +11,49 @@ describe('<Stepper />', () => {
     { title: 'Step 3', description: 'Description 3' },
   ];
 
-  it('should render', () => {
+  const stepsWithIcons = [
+    {
+      title: 'Step 1',
+      content: 'Description 1',
+      icon: <i className="junipero-icons">add_user</i>,
+    },
+    {
+      title: 'Step 2',
+      content: 'Description 2',
+      icon: <i className="junipero-icons">user_edit</i>,
+    },
+    {
+      title: 'Step 3',
+      content: 'Description 3',
+      icon: <i className="junipero-icons">user_valid</i>,
+    },
+  ];
+
+  it('should render steps prop with each status', () => {
     const { unmount, container } = render(
-      <Stepper steps={steps} currentStep={0} />
+      <Stepper steps={steps} active={1} />
     );
     expect(container).toMatchSnapshot();
     unmount();
   });
 
-  it('should display an icon if the parameter is passed', () => {
+  it('should render step children with each status', () => {
+    const { unmount, container } = render(
+      <Stepper active={1}>
+        <Step title="Step 1">Description 1</Step>
+        <Step title="Step 2">Description 2</Step>
+        <Step title="Step 3">Description 3</Step>
+      </Stepper>
+    );
+    expect(container).toMatchSnapshot();
+    unmount();
+  });
+
+  it('should display a different icon per step', () => {
     const { unmount, container } = render(
       <Stepper
-        steps={steps}
-        currentStep={0}
+        steps={stepsWithIcons}
+        active={0}
         icon={<i className="junipero-icons">check_circle</i>}
       />
     );
