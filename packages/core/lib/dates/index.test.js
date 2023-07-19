4b13abe77293da62a504a8d7e48a1d8b0e22d4d3
Ugo Stephant <ugo@poool.fr>
2023-07-19 11:37:56 +0200

    tests(core): add date tests

--- packages/core/lib/dates/index.test.js
+++ packages/core/lib/dates/index.test.js
@@ -1,6 +1,14 @@
 import {
   addMonths,
   subMonths,
+  startOfYear,
+  endOfYear,
+  startOfMonth,
+  endOfMonth,
+  startOfDay,
+  endOfDay,
+  getDaysInMonth,
+  closestIndexTo,
 } from './';
 
 describe('dates', () => {
@@ -43,6 +51,128 @@ describe('dates', () => {
     'less days than current', () => {
       expect(subMonths(new Date(2022, 2, 31), 1).getDate()).toBe(28);
       expect(subMonths(new Date(2022, 2, 31), 1).getMonth()).toBe(1);
+    });
+  });
+
+  describe('startOfYear(date)', () => {
+    it('should return the start of the year', () => {
+      const date = startOfYear(new Date(2020, 10, 1, 12, 30, 30, 300));
+
+      expect(date.getMilliseconds()).toBe(0);
+      expect(date.getSeconds()).toBe(0);
+      expect(date.getMinutes()).toBe(0);
+      expect(date.getHours()).toBe(0);
+      expect(date.getDate()).toBe(1);
+      expect(date.getMonth()).toBe(0);
+      expect(date.getFullYear()).toBe(2020);
+    });
+  });
+
+  describe('endOfYear(date)', () => {
+    it('should return the end of the year', () => {
+      const date = endOfYear(new Date(2020, 10, 1, 12, 30, 30, 300));
+
+      expect(date.getMilliseconds()).toBe(999);
+      expect(date.getSeconds()).toBe(59);
+      expect(date.getMinutes()).toBe(59);
+      expect(date.getHours()).toBe(23);
+      expect(date.getDate()).toBe(31);
+      expect(date.getMonth()).toBe(11);
+      expect(date.getFullYear()).toBe(2020);
+    });
+  });
+
+  describe('startOfMonth(date)', () => {
+    it('should return the start of the month', () => {
+      const date = startOfMonth(new Date(2020, 10, 1, 12, 30, 30, 300));
+
+      expect(date.getMilliseconds()).toBe(0);
+      expect(date.getSeconds()).toBe(0);
+      expect(date.getMinutes()).toBe(0);
+      expect(date.getHours()).toBe(0);
+      expect(date.getDate()).toBe(1);
+      expect(date.getMonth()).toBe(10);
+      expect(date.getFullYear()).toBe(2020);
+    });
+  });
+
+  describe('endOfMonth(date)', () => {
+    it('should return the end of the month', () => {
+      const date = endOfMonth(new Date(2020, 10, 1, 12, 30, 30, 300));
+
+      expect(date.getMilliseconds()).toBe(999);
+      expect(date.getSeconds()).toBe(59);
+      expect(date.getMinutes()).toBe(59);
+      expect(date.getHours()).toBe(23);
+      expect(date.getDate()).toBe(30);
+      expect(date.getMonth()).toBe(10);
+      expect(date.getFullYear()).toBe(2020);
+    });
+  });
+
+  describe('startOfDay(date)', () => {
+    it('should return the start of the day', () => {
+      const date = startOfDay(new Date(2020, 10, 8, 12, 30, 30, 300));
+
+      expect(date.getMilliseconds()).toBe(0);
+      expect(date.getSeconds()).toBe(0);
+      expect(date.getMinutes()).toBe(0);
+      expect(date.getHours()).toBe(0);
+      expect(date.getDate()).toBe(8);
+      expect(date.getMonth()).toBe(10);
+      expect(date.getFullYear()).toBe(2020);
+    });
+  });
+
+  describe('endOfDay(date)', () => {
+    it('should return the end of the day', () => {
+      const date = endOfDay(new Date(2020, 10, 8, 12, 30, 30, 300));
+
+      expect(date.getMilliseconds()).toBe(999);
+      expect(date.getSeconds()).toBe(59);
+      expect(date.getMinutes()).toBe(59);
+      expect(date.getHours()).toBe(23);
+      expect(date.getDate()).toBe(8);
+      expect(date.getMonth()).toBe(10);
+      expect(date.getFullYear()).toBe(2020);
+    });
+  });
+
+  describe('getDaysInMonth(date)', () => {
+    it('should return the number of days in a month', () => {
+      expect(getDaysInMonth(new Date(2020, 0, 1))).toBe(31);
+      expect(getDaysInMonth(new Date(2020, 1, 1))).toBe(29);
+      expect(getDaysInMonth(new Date(2020, 2, 1))).toBe(31);
+      expect(getDaysInMonth(new Date(2020, 3, 1))).toBe(30);
+      expect(getDaysInMonth(new Date(2020, 4, 1))).toBe(31);
+      expect(getDaysInMonth(new Date(2020, 5, 1))).toBe(30);
+      expect(getDaysInMonth(new Date(2020, 6, 1))).toBe(31);
+      expect(getDaysInMonth(new Date(2020, 7, 1))).toBe(31);
+      expect(getDaysInMonth(new Date(2020, 8, 1))).toBe(30);
+      expect(getDaysInMonth(new Date(2020, 9, 1))).toBe(31);
+      expect(getDaysInMonth(new Date(2020, 10, 1))).toBe(30);
+      expect(getDaysInMonth(new Date(2020, 11, 1))).toBe(31);
+    });
+  });
+
+  describe('closestIndexTo(date, dates)', () => {
+    it('should return the index of the closest date', () => {
+      const dates = [
+        new Date(2020, 0, 1),
+        new Date(2020, 0, 2),
+        new Date(2020, 0, 3),
+        new Date(2020, 0, 4),
+        new Date(2020, 0, 5),
+      ];
+
+      expect(closestIndexTo(new Date(2020, 0, 1), dates)).toBe(0);
+      expect(closestIndexTo(new Date(2020, 0, 2), dates)).toBe(1);
+      expect(closestIndexTo(new Date(2020, 0, 3), dates)).toBe(2);
+      expect(closestIndexTo(new Date(2020, 0, 4), dates)).toBe(3);
+      expect(closestIndexTo(new Date(2020, 0, 5), dates)).toBe(4);
+      expect(closestIndexTo(new Date(2020, 0, 6), dates)).toBe(4);
+      expect(closestIndexTo(new Date(2020, 0, 2, 12), dates)).toBe(1);
+      expect(closestIndexTo(new Date(2020, 0, 3, 12, 30), dates)).toBe(3);
     });
   });
 });
