f13c16e0f870a45038fca1478ec76d370dc024e1
maxime Da Silva <maximed@poool.fr>
2023-10-12 11:41:50 +0200

    fix(icons): set icons props to object and not plain string

--- packages/react/lib/icons.d.ts
+++ packages/react/lib/icons.d.ts
@@ -1,33 +1,33 @@
 import { ReactNode } from 'react';
 
-export function Arrows(
-  className?: string,
-): ReactNode | JSX.Element;
+export function Arrows({
+  className,
+}: { className?: string}): ReactNode | JSX.Element;
 
-export function Remove(
-  className?: string,
-): ReactNode | JSX.Element;
+export function Remove({
+  className,
+}: { className?: string}): ReactNode | JSX.Element;
 
-export function ArrowLeft(
-  className?: string,
-): ReactNode | JSX.Element;
+export function ArrowLeft({
+  className,
+}: { className?: string}): ReactNode | JSX.Element;
 
-export function ArrowRight(
-  className?: string,
-): ReactNode | JSX.Element;
+export function ArrowRight({
+  className,
+}: { className?: string}): ReactNode | JSX.Element;
 
-export function ArrowDown(
-  className?: string,
-): ReactNode | JSX.Element;
+export function ArrowDown({
+  className,
+}: { className?: string}): ReactNode | JSX.Element;
 
-export function ArrowUp(
-  className?: string,
-): ReactNode | JSX.Element;
+export function ArrowUp({
+  className,
+}: { className?: string}): ReactNode | JSX.Element;
 
-export function Time(
-  className?: string,
-): ReactNode | JSX.Element;
+export function Time({
+  className,
+}: { className?: string}): ReactNode | JSX.Element;
 
-export function Check(
-  className?: string,
-): ReactNode | JSX.Element;
+export function Check({
+  className,
+}: { className?: string}): ReactNode | JSX.Element;
