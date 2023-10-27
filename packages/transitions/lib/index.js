8234958eeda91761da66e5e82c91eb1a990d99c6
Ugo Stephant <ugo@poool.fr>
2023-10-27 16:29:54 +0200

    feat: add new transitions

--- packages/transitions/lib/index.js
+++ packages/transitions/lib/index.js
@@ -1,45 +1,34 @@
 import { Transition } from '@junipero/react';
 
-export const animateMenu = (
+export const animate = (
   name,
   { time = 100, key, ...opts } = {}
-) =>
-  (menu, { opened, key: k, ...props } = {}) => (
-    <Transition
-      in={opened}
-      mountOnEnter={true}
-      unmountOnExit={true}
-      timeout={time}
-      name={name}
-      children={menu}
-      { ...opts }
-      { ...props }
-      key={k ?? key}
-    />
-  );
+) => (menu, { opened, key: k, ...props } = {}) => (
+  <Transition
+    in={opened}
+    mountOnEnter={true}
+    unmountOnExit={true}
+    timeout={time}
+    name={name}
+    children={menu}
+    { ...opts }
+    { ...props }
+    key={k ?? key}
+  />
+);
 
-export const slideInUpMenu = animateMenu('jp-slide-in-up-menu');
-export const slideInDownMenu = animateMenu('jp-slide-in-down-menu');
+export const slideInUpMenu = animate('jp-slide-in-up-menu');
+export const slideInDownMenu = animate('jp-slide-in-down-menu');
 
 export const animateModal = (
   name,
-  { time = 300, key, ...opts } = {}
-) =>
-  (modal, { opened, key: k, ...props } = {}) => (
-    <Transition
-      in={opened}
-      mountOnEnter={true}
-      unmountOnExit={true}
-      timeout={time}
-      name={name}
-      children={modal}
-      { ...opts }
-      { ...props }
-      key={k ?? key}
-    />
-  );
+  { time = 300, ...opts } = {}
+) => animate(name, { time, ...opts });
 
 export const slideInUpModal = animateModal('jp-slide-in-up-modal');
 export const slideInLeftModal = animateModal('jp-slide-in-left-modal');
 export const appearBounceModal = animateModal('jp-appear-bounce-modal',
   { time: 200 });
+
+export const slideInUp = animate('jp-slide-in-up', { time: 200 });
+export const slideInDown = animate('jp-slide-in-down', { time: 200 });
