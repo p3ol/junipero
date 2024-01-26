57f664feb5d1c8121102fbf1dae81ee4e27bd25a
Ugo Stephant <ugo@poool.fr>
2024-01-26 16:36:55 +0100

    fix(react-d3-plugin): fix missing bar chart export

--- packages/react-d3-plugin/lib/index.js
+++ packages/react-d3-plugin/lib/index.js
@@ -1,6 +1,7 @@
 export { default as Chart } from './Chart';
 export { default as Curve } from './Curve';
 export { default as Marker } from './Marker';
+export { default as Bar } from './Bar';
 
 export {
   useChart,
