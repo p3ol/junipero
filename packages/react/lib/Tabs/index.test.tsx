b1a1033b4a1ea872065ad8bd4a6ee8f2f9be5d42
maxime Da Silva <maximed@poool.fr>
2024-02-29 11:00:37 +0100

    feat(typescript): switch to tsx snapshots

--- packages/react/lib/Tabs/index.test.tsx
+++ packages/react/lib/Tabs/index.test.tsx
@@ -39,7 +39,7 @@ describe('<Tabs />', () => {
     unmount();
   });
 
-  it.only('should allow to render tabs from prop', () => {
+  it('should allow to render tabs from prop', () => {
     const onToggle = jest.fn();
     const { container, getByText, unmount } = render(
       <Tabs
