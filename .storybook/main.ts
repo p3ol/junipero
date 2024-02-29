eb25f2a251d9e90aeb14ac019333f1570da3a3a9
Ugo Stephant <ugo@poool.fr>
2024-02-29 17:30:15 +0100

    chore: fix storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,13 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
 import path from 'node:path';
 
+import type { StorybookConfig } from '@storybook/react-webpack5';
 import postcss from 'postcss';
 import sass from 'sass';
 
 const config: StorybookConfig = {
   stories: [
-    '../packages/**/lib/**/*.stories.{js,tsx}'
+    '../packages/**/lib/**/*.stories.{js,tsx}',
   ],
-
   addons: [
     '@storybook/addon-storysource',
     '@storybook/addon-actions',
@@ -26,19 +25,47 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {},
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
   },
   webpackFinal: (config) => {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
       ...config.resolve?.alias,
-      '@junipero/react': path.resolve(__dirname, '../packages/react/lib/index.ts'),
-      '@junipero/core': path.resolve(__dirname, '../packages/core/lib/index.ts'),
-      '@junipero/transitions': path.resolve(__dirname, '../packages/transitions/lib/index.tsx'),
-      '@junipero/hooks': path.resolve(__dirname, '../packages/hooks/lib/index.ts'),
+      '@junipero/react': path
+        .resolve(__dirname, '../packages/react/lib/index.ts'),
+      '@junipero/core': path
+        .resolve(__dirname, '../packages/core/lib/index.ts'),
+      '@junipero/transitions': path
+        .resolve(__dirname, '../packages/transitions/lib/index.tsx'),
+      '@junipero/hooks': path
+        .resolve(__dirname, '../packages/hooks/lib/index.ts'),
     };
+
     return config;
   },
+  swc: config => ({
+    ...config,
+    jsc: {
+      ...config.jsc,
+      transform: {
+        ...config.jsc?.transform,
+        react: {
+          ...config.jsc?.transform?.react,
+          runtime: 'automatic',
+        },
+      },
+      parser: {
+        ...config.jsc?.parser,
+        syntax: 'typescript',
+        tsx: true,
+        jsx: true,
+      },
+    },
+  })
 };
 
 export default config;
