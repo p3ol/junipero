9676fb3e9f61d6a6ca862d44a6b8f806c19ede45
Ugo Stephant <ugo@poool.fr>
2024-06-06 15:14:40 +0200

    fix(react-d3-plugin): missing type

--- packages/react-d3-plugin/lib/index.ts
+++ packages/react-d3-plugin/lib/index.ts
@@ -2,7 +2,7 @@ export { default as Chart, type ChartProps } from './Chart';
 export { default as Curve, type CurveProps } from './Curve';
 export { default as Marker, type MarkerProps } from './Marker';
 export { default as Bar, type BarProps } from './Bar';
-export { default as Axis, type AxisProps, AxisObject } from './Axis';
+export { default as Axis, type AxisProps, type AxisObject } from './Axis';
 
 export {
   useChart,
