9a4a93da50b63465bca2bcaf6080c0daeec0760c
Ugo Stephant <ugo@poool.fr>
2025-07-11 09:53:36 +0200

    chore: fix tailwind bad pattern

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import junipero from './packages/tailwind-plugin/lib/index';
 
 export default {
   content: [
-    './packages/**/lib/**/*.stories.{jsx,tsx}',
+    './packages/*/lib/**/*.stories.{jsx,tsx}',
   ],
   plugins: [
     junipero,
