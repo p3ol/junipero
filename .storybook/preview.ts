5678b5715444d03e5d413a5eb2623f8da021b32d
Ugo Stephant <ugo@poool.fr>
2025-07-11 09:53:46 +0200

    chore: fix missing storybook dark mode

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,1 +1,27 @@
+import { Preview } from '@storybook/react-webpack5';
+import { withThemeByClassName } from '@storybook/addon-themes';
+
 import './index.sass';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+};
+
+export default preview;
